cdcff51aca399c5f47e4bb5ef85f5369d7fcb2d0
jeffery <jiang1125712@163.com>
2024-01-09 18:24:55 +0800

    路由调整

--- servers/monitor/router/routes/walkingfunny.js
+++ servers/monitor/router/routes/walkingfunny.js
@@ -3,6 +3,6 @@ const { ProjectController, CustomerPVController } = require("../../controllers/c
 module.exports = (router) => {
   /** */
   // 获取项目各项数据，项目数量，健康分等等
-  router.get('/walkingfunny/getProjectBaseCountInfo', ProjectController.getProjectBaseCountInfo);
+  router.post('/walkingfunny/getProjectBaseCountInfo', ProjectController.getProjectBaseCountInfo);
   router.post('/walkingfunny/getProjectCountInfoInRealTime', CustomerPVController.getProjectCountInfoInRealTime);
 }
